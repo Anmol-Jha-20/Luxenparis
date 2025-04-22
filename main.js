01e09d15e9b10b5d79b585e9007d929bc340e41d
Anmol Jha <anmoljha080@gmail.com>
2025-04-22 23:06:46 +0530

    Adding Animation

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const list = document.getElementById("navlist");
 burger.addEventListener("click", () => {
   list.classList.toggle("navlist-active");
 });
+
+const images = [
+  "images/topNeon.png",
+  "images/bubblegum.png",
+  "images/oceara.png",
+  "images/satisfya.png",
+];
+
+let index = 0;
+const heroImage = document.getElementById("hero-image");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  heroImage.classList.add("fade-out");
+  setTimeout(() => {
+    heroImage.src = images[index];
+    heroImage.classList.remove("fade-out");
+    heroImage.classList.add("fade-in");
+  }, 400);
+  setTimeout(() => {
+    heroImage.classList.remove("fade-in");
+  }, 800);
+}, 3000); // Change every 3 seconds
